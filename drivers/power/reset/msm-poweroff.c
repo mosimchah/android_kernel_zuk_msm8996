a1a8bc51f37e4cedadaf09c61ae34547bb321c77
Álvaro Brey <alvaro.brv@gmail.com>
2017-08-18 17:02:11 +0200

    msm-poweroff: Fix useless conditional for warm reset
     * need_warm_reset is true if CONFIG_MSM_PRESERVE_MEM is set

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -295,7 +295,7 @@ static void msm_restart_prepare(const char *cmd)
 	if (need_warm_reset) {
 		qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);
 	} else {
-		qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);
+		qpnp_pon_system_pwr_off(PON_POWER_OFF_HARD_RESET);
 	}
 
 	if (cmd != NULL) {
